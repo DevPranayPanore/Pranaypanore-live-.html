add0e10e183d78eccf7ed4d51c8bc7d7f2aa3023
DevPranayPanore <pranaypanore2@gmail.com>
2026-01-27 20:09:38 +0530

    Add files via upload

---
+++ Pages/Java-script/array--/array.js
@@ -0,0 +1,19 @@
+//CREATE ARRAY 
+let numbers = [10, 20, 30, 40, 50];
+
+//index 0,1,2,3,4
+
+let strings = ["string1", "string2", "string3"];
+let answers = [true, false];
+
+//ARRAY is collection of similar data type
+let array = ["string", 10]
+
+//ARRAY Head
+
+console.log(numbers);
+console.log(typeof (numbers[2]));
+
+console.log(numbers[2], strings[1]);
+
+
--- Pages/Java-script/types-of-function/arrow-function.js
+++ Pages/Java-script/types-of-function/arrow-function.js
@@ -1,11 +1,11 @@
-const add= ( a,b )=>{
-    console.log(a+b)
-    console.log(add(10,20))
+const add = (a, b) => {
+    console.log(a + b)
+    console.log(add(10, 20))
 }
 
 
 
-setTimeout(function(){
+setTimeout(function () {
     console.log("The run after 5 second")
-},5000
+}, 5000
 )
