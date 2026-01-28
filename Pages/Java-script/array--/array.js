d2d2cdaebfc691128642e771b0ec2382b9eb23cc
DevPranayPanore <pranaypanore2@gmail.com>
2026-01-28 20:01:01 +0530

    Add files via upload

--- Pages/Java-script/array--/array.js
+++ Pages/Java-script/array--/array.js
@@ -16,4 +16,28 @@ console.log(typeof (numbers[2]));
 
 console.log(numbers[2], strings[1]);
 
+numbers[0] = 50;
+numbers[1] = 60;
+numbers[2] = 70;
+numbers[3] = 80;
+array[1]="Pranay";
+console.log(numbers);
+console.log(array)
+
+
+
+//array push 
+numbers.push(200);
+console.log("using push function")
+console.log(numbers);
+
+
+//array shift
+numbers.unshift(100);
+console.log("using shift function")
+console.log(numbers);
+
+
+console.log("pranay",numbers)
+
 
